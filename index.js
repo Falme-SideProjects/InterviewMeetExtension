419cde976fe17f9fec78d88e65fc7b2fed67493a
Falme Streamless <falme_streamless@hotmail.com>
2021-04-12 23:55:20 -0300

    Replaced some code

--- index.js
+++ index.js
@@ -17,27 +17,30 @@ function OnNumberWindowsChanged()
 {
     windowsLength = document.querySelectorAll('[jscontroller="'+elementWindowID+'"]').length;
     windowsElements = document.querySelectorAll('[jscontroller="'+elementWindowID+'"]');
+
+    /*var parent = windowsElements[0].parentElement;
+
+    for(var a=0; a<windowsElements.length; a++)
+    {
+        parent.innerHTML+="<button interviewmeet='btn-pin' onclick='alert(\"Alerted\")'>AAAAA</button>";
+    }*/
 }
 
 function RefreshWindowsFixedUI()
 {
 
-    //window.setTimeout(function()
-    //{
-        for(var a=0; a<windowsElements.length; a++)
+    for(var a=0; a<windowsElements.length; a++)
+    {
+        if(a<2)
         {
-            if(a<2)
-            {
-                //document.querySelectorAll('[jscontroller="'+elementWindowID+'"] [jscontroller="'+elementPinID+'"]')[a].click();
-                document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.width = "50%";
-                document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.height = "100%";
-                document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.left = positionsForTwoPins[a];
-                document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.top = "0";
-            } else {
-                document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.display="none";
-            }
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.width = "50%";
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.height = "100%";
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.left = positionsForTwoPins[a];
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.top = "0";
+        } else {
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.display="none";
         }
-    //}, 500);
+    }
 }
 
 function Update()

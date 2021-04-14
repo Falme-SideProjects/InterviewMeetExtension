56cbab05b96dee144cb9de0f6434636ad4715b57
Falme Streamless <falme_streamless@hotmail.com>
2021-04-13 23:56:39 -0300

    Now user can select which one will be fixed in screen

--- background.js
+++ background.js
@@ -1,24 +1,95 @@
-chrome.contextMenus.removeAll();
-chrome.contextMenus.create({
-      title: "List Users",
-      contexts: ["browser_action"],
-      onclick: function() {
-        sendFirst();
-    }
-});
+var lastListOfUsers = new Array();
 
-function sendFirst()
+CreateMainOption();
+
+function CallListOfParticipants()
 {
+    CreateMainOption();
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         chrome.tabs.sendMessage(tabs[0].id, {action: "request_users"}, function(response) {
             
-            var mesRes = "";
+            CreateListOfParticipants(response.farewell);
+
+            /*var mesRes = "";
             for(var a=0; a<response.farewell.length; a++)
             {
                 mesRes+= response.farewell[a]+",";
             }
+            
+            alert(mesRes);*/
+        });  
+    });
+}
 
-            alert(mesRes);
+function ClearList()
+{
+    chrome.contextMenus.removeAll();
+}
+
+function CreateMainOption()
+{
+    ClearList();
+    chrome.contextMenus.create({
+          title: "List Users",
+          contexts: ["browser_action"],
+          id:"loadPeople"
+          /*onclick: function() {
+            CallListOfParticipants();
+        }*/
+    });
+}
+
+function CreateListOfParticipants(list)
+{
+    chrome.contextMenus.create({
+        title: "List Of People",
+        contexts: ["browser_action"],
+        id:"parent"
+    });
+
+    lastListOfUsers = list;
+    for(var a=0; a<list.length; a++)
+    {
+        chrome.contextMenus.create({
+            title: list[a],
+            parentId: "parent",
+            id:("person"+a),
+            contexts: ["browser_action"]
+        });
+    }
+}
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "loadPeople") {
+        CallListOfParticipants();
+    }
+    
+    if(info.menuItemId.includes('person'))
+    {
+        var numberID = info.menuItemId[info.menuItemId.length-1];
+        var intNumberID = Number.parseInt(numberID, 16);
+        
+        //alert(lastListOfUsers[intNumberID]);
+        ActionOnUser(intNumberID);
+    }
+});
+
+function ActionOnUser(user)
+{
+    alert(lastListOfUsers[user]);
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {action: "fix_user", id:user}, function(response) {
+            
+            //CreateListOfParticipants(response.farewell);
+
+            /*var mesRes = "";
+            for(var a=0; a<response.farewell.length; a++)
+            {
+                mesRes+= response.farewell[a]+",";
+            }
+            
+            alert(mesRes);*/
         });  
     });
 }
--- index.js
+++ index.js
@@ -28,10 +28,28 @@ function OnNumberWindowsChanged()
 
 function RefreshWindowsFixedUI()
 {
+    if(listOfCalledElementsToPin.length < 2) return;
 
     for(var a=0; a<windowsElements.length; a++)
     {
-        if(a<2)
+        if(a==listOfCalledElementsToPin[0] || a==listOfCalledElementsToPin[1])
+        {
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.width = "50%";
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.height = "100%";
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.left = positionsForTwoPins[a];
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.top = "0";
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.opacity = "1";
+        } else {
+            document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.display="none";
+        }
+    }
+}
+
+function FixUserById(id)
+{
+    for(var a=0; a<windowsElements.length; a++)
+    {
+        if(a==id)
         {
             document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.width = "50%";
             document.querySelectorAll('[jscontroller="'+elementWindowID+'"]')[a].style.height = "100%";
@@ -54,13 +72,21 @@ function Update()
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message);
-    var responseArray = new Array();
-    for(var a=0; a<windowsElements.length; a++)
+
+    if(message.action == "request_users")
     {
+        var responseArray = new Array();
+        
+        for(var a=0; a<windowsElements.length; a++)
         responseArray[a] = windowsElements[a].querySelectorAll('[jscontroller="GQnsGd"]')[0].innerHTML;
-        //[jscontroller="GQnsGd"]
+        
+        sendResponse({farewell:responseArray});
     }
-    sendResponse({farewell:responseArray});
+    else if(message.action == "fix_user")
+    {
+        if(listOfCalledElementsToPin.length == 2) listOfCalledElementsToPin = new Array();
+        listOfCalledElementsToPin.push(message.id);
+    }
     return true
 });
 
